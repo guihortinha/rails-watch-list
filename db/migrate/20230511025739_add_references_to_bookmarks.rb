08ee926f445869e491cc59d39e5af7f496a6b05c
Guilherme Hortinha <guihortinha@gmail.com>
2023-05-11 17:06:46 +0900

    first v

---
+++ db/migrate/20230511025739_add_references_to_bookmarks.rb
@@ -0,0 +1,8 @@
+class AddReferencesToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :bookmarks,:movie_id,:integer
+    remove_column :bookmarks,:list_id,:integer
+    add_reference :bookmarks, :movie, null: false, foreign_key: true
+    add_reference :bookmarks, :list, null: false, foreign_key: true
+  end
+end
